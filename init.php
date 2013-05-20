477374df81414d94d368591a7faf19414ca4683f
Luc Didry <luc@didry.org>
2013-05-20 19:04:02 +0200

    Mise à jour pour l'API de ttrss 1.7.9

--- init.php
+++ init.php
@@ -5,24 +5,24 @@ class framarticle_toolbar extends Plugin {
 	private $host;
 
 	function about() {
-		return array(0.1,
+		return array(0.2,
 			"Toolbar for easy access to feed functions, based on https://github.com/idoxlr8/article_toolbar",
 			"ldidry", false);
 	}
 
 	function init($host) {
-		$this->link = $host->get_link();
+		$this->dbh = $host->get_dbh();
 		$this->host = $host;
 
 		$host->add_hook($host::HOOK_TOOLBAR_BUTTON, $this);
 	}
 	///Experimental get users name
-    function CDM_EXPANDED($link) {
+    function CDM_EXPANDED() {
         if (!$user_id) {
             $user_id = $_SESSION["uid"];
         }
-        $result = db_query($link, "SELECT CDM_EXPANDED FROM ttrss_user_prefs WHERE owner_uid = $user_id");	
-        $value = db_fetch_result($result, 0, "CDM_EXPANDED");
+        $result = db_query("SELECT CDM_EXPANDED FROM ttrss_user_prefs WHERE owner_uid = $user_id");	
+        $value  = db_fetch_result($result, 0, "CDM_EXPANDED");
         return $value;
     }	
 
@@ -33,5 +33,9 @@ class framarticle_toolbar extends Plugin {
 	function HOOK_TOOLBAR_BUTTON() {
 		require_once dirname(__FILE__) . "/toolbar.php";
 	}
+
+    function api_version() {
+      return 2;
+    }
 }
 ?>
